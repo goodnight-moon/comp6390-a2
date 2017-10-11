2b5e784aa33318f968b3b0dbe9da61a01356ce69
wacky6.AriesMBP <416707889@qq.com>
2017-10-11 15:33:00 +1100

    add images

--- src/components/pages/change-hands.vue
+++ src/components/pages/change-hands.vue
@@ -1,5 +1,5 @@
 <template>
-  <Timeline time=" " location="Australia" :progress="progress">
+  <Timeline time="1890 - 1906" location="Australia" :progress="progress">
 
     <p>
       The telescope then passed through the hands of a number of Australia’s amateur astronomers.
@@ -8,6 +8,17 @@
     <p>
       Fill in more content!
     </p>
+
+    <div slot="detail">
+      <h3>List of funny people that owned this telescope. (Purly Imaginary)</h3>
+      <h4 class="del">Actually, the people that make us do this assignment.</h4>
+      <ul>
+        <li>... Duncan ...</li>
+        <li>... Stevenson ...</li>
+      </ul>
+
+      <img src="https://researchers.anu.edu.au/researchers/stevenson-dr/image" alt="Duncan Stevenson"></img>
+    </div>
 
     <div class="background" style="background-color: blue" slot="background"></div>
 
@@ -30,5 +41,6 @@ export default {
 </script>
 
 <style lang="stylus">
-    
+.del
+  text-decoration: line-through
 </style>
