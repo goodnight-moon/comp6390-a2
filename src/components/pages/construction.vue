7676d416453b372bcc55f6736302d5684fb9fc47
wacky6.AriesMBP <416707889@qq.com>
2017-10-11 15:33:17 +1100

    change show more page

--- src/components/pages/construction.vue
+++ src/components/pages/construction.vue
@@ -9,8 +9,6 @@
       Macdonnell imported the telescope to Port Macquarie in New South Wales. There he installed it in a purpose-built observatory behind the Bank of New South Wales, where he worked as manager, and turned it to the heavens.
     </p>
 
-    <p slot="detail">More info....</p>
-
     <div class="background" style="background-color: blue" slot="background"></div>
 
   </Timeline>
